028fd2e1f03ba91ce0e47e463612352fff0000df
Paweł Baster <pabaster@gmail.com>
2015-07-22 11:05:51 +0200

    temporarily use minified version only since the non-minified is causing problems.

--- JsTrans.php
+++ JsTrans.php
@@ -91,11 +91,9 @@ class JsTrans
             }
         }
 
-        $jsTransFile = YII_DEBUG ? 'JsTrans.min.js' : 'JsTrans.js';
-
         Yii::app()->getClientScript()->addPackage('JsTrans', [
             'baseUrl' => $this->_publishUrl,
-            'js' => [$jsTransFile, $dictionaryFile],
+            'js' => ['JsTrans.min.js', $dictionaryFile],
         ]);
 
         Yii::app()->getClientScript()->registerPackage('JsTrans');
